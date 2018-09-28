9e97217f49a3f82d2d849671e029a8e2d28b25e7
Любовь Яронская <luba.yaronskaya@gmail.com>
2018-09-28 18:46:45 +0300

    Update test_q.js

--- test_q.js
+++ test_q.js
@@ -23,3 +23,11 @@ var debugInput = document.querySelector("input");
      }
      debugInput.addEventListener("click", updateDebugState);
      updateDebugState();
+
+
+var debugInput = document.querySelector("input");
+     function updateDebugState() {
+         document.body.classList.toggle('debug-on', debugInput.checked);
+     }
+     debugInput.addEventListener("click", updateDebugState);
+     updateDebugState();
