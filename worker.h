9f69915e053d54ba80d1408f06f94c7adffb4eda
Любовь Яронская <luba.yaronskaya@gmail.com>
2018-10-01 11:42:26 +0300

    Update worker.h

--- worker.h
+++ worker.h
@@ -1,135 +1,209 @@
-#ifndef MASTER_H
-#define MASTER_H
+#ifndef WORKER_H
+#define WORKER_H
 
-#include "Worker.h"
+#include <mpi.h>
+#include "Field.h"
 #include "CommandException.h"
+#include "utility.h"
 
-class Master {
-public:
-    enum StateType
-    {
-        NOT_STARTED,
-        NOT_RUNNING,
-        RUNNING,
-        STOPPED,
-        FINISHED
-    };
-    
-    Field* life_field;
-    
-private:
-    StateType state;
-    unsigned int workersCount;
-    bool* field_buffer;
-    int worker_arg[2];
-    int iterNumber;
-    
-public:
-    Master() : state(NOT_STARTED) { life_field = new Field; }
-    void init_workers();
-    void run_workers(int iterations);
-    void stop_workers();
-    void shutdown();
-    void gather_field();
-    void change_state(StateType s);
-    StateType get_state();
-    int get_iter_number();
-    Field* get_field();
-    void set_workers_count(int workers_count);
-    int get_workers_count();
+enum MESSAGE_TYPE {
+    START,
+    RUN,
+    STOP,
+    FINISH,
+    FIELD_INFO,
+    FIELD_INIT,
+    FIELD_FULL,
+    FIELD_GATHER,
+    ITERATION_GATHER,
+    ROW_SENDRECV
 };
 
-void Master::set_workers_count(int workers_count) {
-    workersCount = workers_count;
-}
-
-int Master::get_workers_count() {
-    return workersCount;
-}
-
-void Master::init_workers() {
-    field_buffer = new bool[life_field->width * life_field->height];
-    worker_arg[0] = life_field->width / workersCount;
-    worker_arg[1] = life_field->height;
-    cout << worker_arg[0] << " " << worker_arg[2] << " " << workersCount << endl;
-    int start_row = 0;
+class Worker {
+public:
+    void worker_function(int rank, int comm_size);
+    Worker();
     
-    for (int i = 1; i <= workersCount; ++i) {
-        if (i == workersCount) {
-            worker_arg[0] += life_field->width % workersCount;
-        }
-        life_field->write_to_buffer(field_buffer, start_row, worker_arg[0]);
-        
-        MPI_Send(worker_arg, 2, MPI::INT, i, FIELD_INFO, MPI_COMM_WORLD);
-        MPI_Send(field_buffer, worker_arg[0] * life_field->height, MPI::BOOL,
-                 i, FIELD_INIT, MPI_COMM_WORLD);
-        cout << "sended info\n";
-        start_row += worker_arg[0];
-    }
-}
-
-void Master::gather_field() {
-    MPI_Status status;
-    bool* field_part_pointer = field_buffer;
-    bool some_message = 0;
-    worker_arg[0] = life_field->width / workersCount;
-
-    for (int i = workersCount; i >= 1; --i) {
-        if (i == workersCount) {
-            worker_arg[0] += life_field->width % workersCount;
-        }
-        
-        MPI_Send(&some_message, 1, MPI::INT, i, FIELD_GATHER, MPI_COMM_WORLD);
-        MPI_Recv(field_part_pointer, worker_arg[0] * life_field->height, MPI::BOOL, i, FIELD_GATHER, MPI_COMM_WORLD, &status);
-        field_part_pointer += worker_arg[0] * life_field->height;
-    }
-    life_field->init_from_buffer(field_buffer, life_field->width, life_field->height);
-}
-
-int Master::get_iter_number() {
-    return iterNumber;
-}
-
-Field* Master::get_field() {
-    return life_field;
-}
-
-void Master::run_workers(int iterations) {
-    for (int i = 1; i <= workersCount; ++i) {
-        MPI_Send(&iterations, 1, MPI::INT, i, RUN, MPI_COMM_WORLD);
-    }
-}
-
-void Master::change_state(StateType s) {
-    state = s;
-}
-
-Master::StateType Master::get_state() {
-    return state;
-}
-
-void Master::stop_workers() {
-    MPI_Status status;
-    bool some_message = 0;
-    cout << "Master starts stopping workers\n";
-    MPI_Send(&some_message, 1, MPI::INT, 1, STOP, MPI_COMM_WORLD);
-    cout << "Master sent some info\n";
-    int new_iter_number;
+private:
+    int id;
+    int workersCount;
+    Field* field;
+    Field* prev_field;
+    bool* field_buffer;
+    int field_height;
+    int field_width;
+    int iterations_todo;
     int iterations_ready;
-    cout << "But previous iterNumber " << iterNumber << endl;
-    for (int i = 2; i <= workersCount; ++i) {
-        MPI_Recv(&iterations_ready, 1, MPI::INT, i, ITERATION_GATHER, MPI_COMM_WORLD, &status);
-        cout << "received from " << i << " " << iterations_ready << endl;
-    }
-    iterNumber = iterations_ready;
+    bool init_stop;
+    bool after_stop;
+    bool received_stop;
+    MPI_Request stop_request;
+    int count_live_neighbors(int x, int y);
+    void perform_field(bool* received_low_row, bool* received_high_row);
+    bool check_break_work();
+    
+};
+
+Worker::Worker() {
+    field = new Field;
+    prev_field = new Field;
+    init_stop = false;
+    after_stop = true;
+    received_stop = false;
 }
 
-void Master::shutdown() {
-    int some_message = 0;
-    for (int i = 1; i <= workersCount; ++i) {
-        MPI_Send(&some_message, 1, MPI::INT, i, FINISH, MPI_COMM_WORLD);
+void Worker::worker_function(int rank, int comm_size) {
+    id = rank;
+    workersCount = comm_size - 1;
+    iterations_todo = 0;
+    iterations_ready = 0;
+    int worker_arg[2];
+    MPI_Status status;
+    MPI_Recv(worker_arg, 2, MPI::INT, 0, FIELD_INFO, MPI_COMM_WORLD, &status);
+    field_width = worker_arg[0];
+    field_height = worker_arg[1];
+    field_buffer = new bool[field_height * (field_width + 2)];
+    
+    MPI_Recv(field_buffer + field_height, field_height * field_width, MPI::BOOL, 0, FIELD_INIT, MPI_COMM_WORLD, &status);
+    
+    field->init_from_buffer(field_buffer, field_width + 2, field_height);
+    prev_field->init_from_buffer(field_buffer, field_width + 2, field_height);
+    
+    int lower_worker_id = (rank == 1) ? (comm_size - 1) : (rank - 1);
+    int higher_worker_id = (rank == comm_size - 1) ? 1 : (rank + 1);
+    bool* lower_row_send = new bool[field_height];
+    bool* higher_row_send = new bool[field_height];
+    bool* lower_row_receive = new bool[field_height];
+    bool* higher_row_receive = new bool[field_height];
+    
+    while(true) {
+        bool curr_status = check_break_work();
+        if (!curr_status)
+            break;
+        
+        received_stop = false;
+        
+        field->write_row(lower_row_send, 1);
+        MPI_Sendrecv(lower_row_send, field_height, MPI::BOOL, lower_worker_id, ROW_SENDRECV,
+                     higher_row_receive, field_height, MPI::BOOL, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+        
+        if (status.MPI_TAG == STOP) {
+            received_stop = true;
+            int max_iteration = int_from_boolarray(higher_row_receive, field_height);
+            iterations_todo = max_iteration - iterations_ready;
+            MPI_Recv(higher_row_receive, field_height, MPI::BOOL, higher_worker_id, ROW_SENDRECV,
+                     MPI_COMM_WORLD, &status);
+
+        }
+        
+        field->write_row(higher_row_send, field_width + 1);
+        MPI_Sendrecv(higher_row_send, field_height, MPI::BOOL, higher_worker_id, ROW_SENDRECV,
+                     lower_row_receive, field_height, MPI::BOOL, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+        
+        if (status.MPI_TAG == STOP) {
+            received_stop = true;
+            int max_iteration = int_from_boolarray(lower_row_receive, field_height);
+            iterations_todo = max_iteration - iterations_ready;
+            MPI_Recv(lower_row_receive, field_height, MPI::BOOL, lower_worker_id, ROW_SENDRECV,
+                     MPI_COMM_WORLD, &status);
+        }
+    
+        perform_field(lower_row_receive, higher_row_receive);
+        iterations_todo--;
+        iterations_ready++;
     }
-    state = FINISHED;
+}
+
+bool Worker::check_break_work() {
+    int message;
+    int flag = false;
+    MPI_Status status;
+    bool iteration_sent = false;
+    
+    while (iterations_todo <= 0) {
+        if (!iteration_sent && iterations_ready > 0) {
+            MPI_Send(&iterations_ready, 1, MPI::INT, 0, ITERATION_GATHER, MPI_COMM_WORLD);
+            iteration_sent = true;
+        }
+        MPI_Recv(&message, 1, MPI::INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+        
+        switch (status.MPI_TAG) {
+            case RUN:
+                iterations_todo += message;
+                after_stop = true;
+                return true;
+            case FINISH:
+                return false;
+            case FIELD_GATHER:
+                field->write_to_buffer(field_buffer, 0, field_width);
+                MPI_Send(field_buffer, field_width * field_height, MPI::BOOL, 0, FIELD_GATHER, MPI_COMM_WORLD);
+                break;
+            case STOP:
+                break;
+        }
+    }
+    
+    if (id == 1 && after_stop) {
+        if (!init_stop) {
+            MPI_Irecv(&message, 1, MPI::INT, 0, STOP, MPI_COMM_WORLD, &stop_request);
+            init_stop = true;
+        }
+        MPI_Test(&stop_request, &flag, &status);
+        if (flag) {
+            init_stop = false;
+            if (iterations_todo > 0) {
+                return true;
+            }
+            for (int i = 2; i <= workersCount; ++i) {
+                bool* iteration_buffer = new bool[field_height];
+                int max_iteration = iterations_ready + iterations_todo;
+                boolarray_from_int(max_iteration, iteration_buffer, field_height);
+                MPI_Send(iteration_buffer, field_height, MPI::BOOL, i, STOP, MPI_COMM_WORLD);
+            }
+            return true;
+        }
+        after_stop = false;
+        flag = false;
+    }
+    
+    return true;
+}
+
+void Worker::perform_field(bool* received_low_row, bool* received_high_row) {
+    for (int i = 0; i < field_height; ++i) {
+        field->data[0][i] = received_low_row[i];
+        field->data[field_width - 1][i] = received_high_row[i];
+    }
+    int p, live_nbs;
+    
+    for (int i = 1; i < field->width - 1; ++i) {
+        for (int j = 0; j < field->height; ++j) {
+            p = prev_field->data[i][j];
+            live_nbs = count_live_neighbors(i, j);
+            if (p == 1) {
+                if (live_nbs < 2 || live_nbs > 3)
+                    field->data[i][j] = 0;
+            } else {
+                if (live_nbs == 3)
+                    field->data[i][j] = 1;
+            }
+        }
+    }
+    std::swap(field, prev_field);
+}
+
+int Worker::count_live_neighbors(int x, int y) {
+    unsigned int count = 0;
+    for (int i = x - 1; i <= x+1; ++i) {
+        for (int j = y - 1; j <= y + 1; ++j) {
+            if (i == x && j == y)
+                continue;
+            if (prev_field->data[i][(j + field_height) % field_height]) {
+                count++;
+            }
+        }
+    }
+    return count;
 }
 
 #endif
